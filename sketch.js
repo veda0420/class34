90d1092809addbacca3eb115374ebf43b2645733
veda0420 <75167659+veda0420@users.noreply.github.com>
2021-04-07 17:57:55 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,43 @@
+var ball;
+var database,position
+function setup(){
+    createCanvas(500,500);
+    ball = createSprite(250,250,10,10);
+    ball.shapeColor = "red";
+    database=firebase.database()
+    var loc=database.ref("ball/position")
+    loc.on("value",readopt,showError)
+}
+function readopt(data){
+    position=data.val()
+    ball.x=position.x
+    ball.y=position.y
+
+}
+function showError(){
+console.log("error")
+}
+function draw(){
+    background("white");
+    if(keyDown(LEFT_ARROW)){
+        changePosition(-1,0);
+    }
+    else if(keyDown(RIGHT_ARROW)){
+        changePosition(1,0);
+    }
+    else if(keyDown(UP_ARROW)){
+        changePosition(0,-1);
+    }
+    else if(keyDown(DOWN_ARROW)){
+        changePosition(0,+1);
+    }
+    drawSprites();
+}
+
+function changePosition(x,y){
+   var ref=database.ref("ball/position")
+   ref.set({
+       x:position.x+x,
+       y:position.y+y
+   })
+}
